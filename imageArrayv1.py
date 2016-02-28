93d26028d2788ab3f8216b6c6084be6184d14863
Tyler Lee <tlee753@gmail.com>
2016-02-28 13:21:46 -0500

    removed junk

--- imageArrayv1.py
+++
@@ -1,9 +0,0 @@
-from numpy import array
-from PIL import Image
-
-def PIL2array(input):
-    img = Image.open(input)
-    arr = array(img)
-    print (arr)
-    
-PIL2array('Hole.jpeg')
--- imageTestv1.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-def imageTestv1 (inputFile):
-    image = Image.open(inputFile)
-    image.show()
-    
-imageTestv1('Hole.jpeg')
