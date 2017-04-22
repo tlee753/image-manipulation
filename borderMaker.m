afed74c9fa6604da5251e834069c99692c95eeef
Tyler Lee <tlee753@gmail.com>
2017-04-23 00:28:07 +0200

    updated border maker matlab script

--- borderMaker.m
+++ borderMaker.m
@@ -1,12 +1,13 @@
-function borderMaker (imageFile)
-% function to create 100 pixel transparent border around an image
+function borderMaker ()
+% function to create  pixel border around an image
+% works with .jpg's and .png's (including png transparency)
 
-borderSize = 150;
+imageFile = input('File name? (In same directory)\n', 's');
+borderSize = input('Border size? (Specify a number of pixels)\n');
 
 [img, ~, alpha] = imread(imageFile);
 
 layer = img(:, :, 1);
-
 [rows, cols] = size(layer);
 
 vertArray = 255 * ones(borderSize, cols);
@@ -17,13 +18,17 @@ horzArray = 255 * ones(rows + 2*borderSize, borderSize);
 horzBorder = cat(3, horzArray, horzArray, horzArray);
 img = [horzBorder, img, horzBorder];
 
-alphaVertArray = zeros(borderSize, cols);
-alpha = [alphaVertArray; alpha; alphaVertArray];
-alphaHorzArray = zeros(rows + 2*borderSize, borderSize);
-alpha = [alphaHorzArray, alpha, alphaHorzArray];
+if (strcmp(imageFile(end-3:end), '.jpg'))
+    newImageName = [imageFile(1:end-4), '_border.jpg'];
+    imwrite(img, newImageName);
+    
+elseif (strcmp(imageFile(end-3:end), '.png'))
+    alphaVertArray = zeros(borderSize, cols);
+    alpha = [alphaVertArray; alpha; alphaVertArray];
+    alphaHorzArray = zeros(rows + 2*borderSize, borderSize);
+    alpha = [alphaHorzArray, alpha, alphaHorzArray];
 
-newImageName = [imageFile(1:end-4), '_border.png'];
-
-imwrite(img, newImageName, 'Alpha', alpha);
+    newImageName = [imageFile(1:end-4), '_border.png'];
+    imwrite(img, newImageName, 'Alpha', alpha);
     
 end
